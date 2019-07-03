ba035906c8d137fb638e8cb708f60cbb8fad8093
Omar Sabry <ohsabry@gmail.com>
2019-07-03 18:40:41 -0400

    add helpers to clone

--- cmd/clone.go
+++ cmd/clone.go
@@ -3,20 +3,11 @@ package cmd
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/exec"
-	"strings"
 
 	"github.com/TouchBistro/tb/config"
+	"github.com/TouchBistro/tb/util"
 	"github.com/spf13/cobra"
 )
-
-func exists(filePath string) bool {
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
 
 var cloneCmd = &cobra.Command{
 	Use:   "clone",
@@ -27,18 +18,14 @@ var cloneCmd = &cobra.Command{
 		services := *config.All()
 		for _, s := range services {
 			path := fmt.Sprintf("./%s", s.Name)
-			if !s.Repo || exists(path) {
+			if !s.Repo || util.FileOrDirExists(path) {
 				continue
 			}
 
 			fmt.Printf("%s is missing. cloning...\n", s.Name)
 
-			cmdStr := fmt.Sprintf("clone git@github.com:TouchBistro/%s.git", s.Name)
-			cmd := exec.Command("git", strings.Fields(cmdStr)...)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-
-			err := cmd.Run()
+			repoURL := fmt.Sprintf("git@github.com:TouchBistro/%s.git", s.Name)
+			err := util.ExecStdoutStderr("git", "clone", repoURL)
 			if err != nil {
 				log.Fatal(err)
 			}
