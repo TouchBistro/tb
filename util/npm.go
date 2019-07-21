3ffbc7d7ffe1e3d1969f8465dd49bd86cea788d4
Christopher Szatmary <cs@christopherszatmary.com>
2019-07-21 14:06:22 -0400

    Convert to new style

--- util/npm.go
+++ util/npm.go
@@ -1,54 +1,58 @@
 package util
 
 import (
-	"errors"
 	"fmt"
 	"os"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 const npmToken = "NPM_TOKEN"
 
 func NpmLogin() error {
-	fmt.Println("Checking private npm repository token...")
+	log.Infoln("Checking private npm repository token...")
 	if os.Getenv(npmToken) != "" {
-		fmt.Printf("Required env var %s is set\n", npmToken)
+		log.Infof("Required env var %s is set\n", npmToken)
 		return nil
 	}
 
-	fmt.Printf("Required env var %s not set\nChecking ~/.npmrc...\n", npmToken)
+	log.Infof("Required env var %s not set\nChecking ~/.npmrc...\n", npmToken)
 
 	npmrcPath := os.Getenv("HOME") + "/.npmrc"
 	if !FileOrDirExists(npmrcPath) {
-		fmt.Println("No ~/.npmrc found.")
-		fmt.Println("Log in to the touchbistro npm registry with command: 'npm login' and try again.")
-		fmt.Println("If this does not work...Create a https://www.npmjs.com/ account called: touchbistro-youremailname, then message DevOps to add you to the @touchbistro account")
+		log.Warnln("No ~/.npmrc found.")
+		log.Warnln("Log in to the touchbistro npm registry with command: 'npm login' and try again.")
+		log.Warnln("If this does not work...Create a https://www.npmjs.com/ account called: touchbistro-youremailname, then message DevOps to add you to the @touchbistro account")
 		// TODO: We could also let them log in here and continue
 		return errors.New("error not logged into npm registry")
 	}
 
-	fmt.Println("Looking for token in ~/.npmrc...")
+	log.Infoln("Looking for token in ~/.npmrc...")
 
 	// figure this thing out
 	token := "" // =$(tail -n1 ~/.npmrc | grep -o '//registry.npmjs.org/:_authToken=.*' | cut -f2 -d=)
 
 	if token == "" {
-		fmt.Println("could not parse authToken out of ~/.npmrc")
+		log.Warnln("could not parse authToken out of ~/.npmrc")
 		return errors.New("error no npm token")
 	}
 
-	fmt.Println("Found authToken. adding to dotfiles and exporting")
-	fmt.Println("...exporting NPM_TOKEN=$token")
-	os.Setenv(npmToken, token)
+	log.Infoln("Found authToken. adding to dotfiles and exporting")
+	log.Infoln("...exporting NPM_TOKEN=$token")
 
-	rcFiles := [2]string{".zshrc", ".bashrc"}
+	rcFiles := [...]string{".zshrc", ".bash_profile"}
 
 	for _, file := range rcFiles {
-		rcPath := os.Getenv("HOME") + "/" + file
-		fmt.Printf("...adding export to %s.\n", rcPath)
-		AppendLineToFile(rcPath, "export NPM_TOKEN="+token)
-		fmt.Println("...done")
+		rcPath := fmt.Sprintf("%s/%s", os.Getenv("HOME"), file)
+		log.Debugf("...adding export to %s.\n", rcPath)
+		err := AppendLineToFile(rcPath, "export NPM_TOKEN="+token)
+		if err != nil {
+			return errors.Wrapf(err, "failed to export to file %s", file)
+		}
+		log.Debugln("...done")
 	}
 
-	fmt.Println("run 'source ~/.zshrc' or 'source ~/.bashrc'")
-	return errors.New("error source rc file") // Why is this a thing?
+	log.Infoln("run 'source ~/.zshrc' or 'source ~/.bash_profile'")
+	return errors.New("error source rc file")
 }
