6c325f48d971808a45b5d4d0e2ccae36bd80c631
Omar Sabry <ohsabry@gmail.com>
2019-07-23 00:48:50 -0400

    improve shitty ecr login message

--- docker/docker.go
+++ docker/docker.go
@@ -21,7 +21,7 @@ var (
 func ECRLogin() error {
 	out, err := exec.Command("aws", strings.Fields("ecr get-login --region us-east-1 --no-include-email")...).Output()
 	if err != nil {
-		return errors.Wrap(err, "aws ecr get-login failed")
+		return errors.Wrap(err, "executing aws ecr get-login failed - try running aws configure.")
 	}
 
 	dockerLoginArgs := strings.Fields(string(out))
