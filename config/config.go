eb02cf78424cc06effd2c57acf3c1a0f1cc9845d
Omar Sabry <ohsabry@gmail.com>
2019-07-29 16:18:39 -0400

    dont need ruby 2.4.6 anymore

--- config/config.go
+++ config/config.go
@@ -167,7 +167,6 @@ func BaseImages() []string {
 		"touchbistro/alpine-node:12-build",
 		"touchbistro/alpine-node:12-runtime",
 		"touchbistro/ubuntu16-ruby:2.5.5-build",
-		"touchbistro/ubuntu16-ruby:2.4.6-build",
 	}
 }
 
