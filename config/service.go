b2d941c78c2d238b55494fa709101efd0044e579
Christopher Szatmary <cs@christopherszatmary.com>
2020-01-06 10:56:54 -0500

    Move expandVars to util

--- config/service.go
+++ config/service.go
@@ -3,7 +3,8 @@ package config
 import (
 	"fmt"
 	"os"
-	"regexp"
+
+	"github.com/pkg/errors"
 
 	"github.com/pkg/errors"
 
@@ -58,31 +59,6 @@ func (s Service) ImageURI() string {
 
 /* Private helpers */
 
-func expandVars(str string, vars map[string]string) string {
-	// Regex to match variable substitution
-	regex := regexp.MustCompile(`\$\{([\w-]+)\}`)
-	indices := regex.FindAllStringSubmatchIndex(str, -1)
-
-	// Go through the string in reverse order and replace all variables with their value
-	expandedStr := str
-	for i := len(indices) - 1; i >= 0; i-- {
-		match := indices[i]
-		// match[0] is the start index of the whole match
-		startIndex := match[0]
-		// match[1] is the end index of the whole match (exclusive)
-		endIndex := match[1]
-		// match[2] is start index of group
-		startIndexGroup := match[2]
-		// match[3] is end index of group (exclusive)
-		endIndexGroup := match[3]
-
-		varName := str[startIndexGroup:endIndexGroup]
-		expandedStr = expandedStr[:startIndex] + vars[varName] + expandedStr[endIndex:]
-	}
-
-	return expandedStr
-}
-
 func parseServices(config ServiceConfig) (ServiceMap, error) {
 	parsedServices := make(ServiceMap)
 
@@ -101,7 +77,7 @@ func parseServices(config ServiceConfig) (ServiceMap, error) {
 		}
 
 		// Expand any docker registry vars
-		service.Remote.Image = expandVars(service.Remote.Image, config.Global.Variables)
+		service.Remote.Image = util.ExpandVars(service.Remote.Image, config.Global.Variables)
 
 		parsedServices[name] = service
 	}
