8f65b4abc5de13a556aa241709acf5b00d926a7c
Christopher Szatmary <cs@christopherszatmary.com>
2020-02-13 21:50:47 -0500

    Revert "Remove no-start-servers"
    This reverts commit 636892f37e0a2c8581c3b696c21eab2a8f359a58.

--- cmd/up.go
+++ cmd/up.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"time"
@@ -21,6 +22,7 @@ import (
 
 type options struct {
 	shouldSkipServicePreRun bool
+	shouldSkipServerStart   bool
 	shouldSkipGitPull       bool
 	shouldSkipDockerPull    bool
 	cliServiceNames         []string
@@ -187,6 +189,12 @@ Examples:
 	tb up --services postgres,localstack`,
 	Args: cobra.NoArgs,
 	PreRun: func(cmd *cobra.Command, args []string) {
+		if opts.shouldSkipServerStart {
+			os.Setenv("START_SERVER", "false")
+		} else {
+			os.Setenv("START_SERVER", "true")
+		}
+
 		err := deps.Resolve(
 			deps.Brew,
 			deps.Aws,
@@ -369,6 +377,7 @@ Examples:
 }
 
 func init() {
+	upCmd.PersistentFlags().BoolVar(&opts.shouldSkipServerStart, "no-start-servers", false, "dont start servers with yarn start or yarn serve on container boot")
 	upCmd.PersistentFlags().BoolVar(&opts.shouldSkipServicePreRun, "no-service-prerun", false, "dont run preRun command for services")
 	upCmd.PersistentFlags().BoolVar(&opts.shouldSkipGitPull, "no-git-pull", false, "dont update git repositories")
 	upCmd.PersistentFlags().BoolVar(&opts.shouldSkipDockerPull, "no-remote-pull", false, "dont get new remote images")
