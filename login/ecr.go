06308c8038f31898647f88f9a514bb96243e809e
damianpursey <dpursey@touchbistro.com>
2020-02-14 12:45:08 -0500

    fix: bezos

--- login/ecr.go
+++ login/ecr.go
@@ -1,9 +1,11 @@
 package login
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io"
 	"os/exec"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecr"
@@ -30,24 +32,30 @@ func (s ECRLoginStrategy) Login() error {
 	}
 
 	authData := *result.AuthorizationData[0]
-	token := *authData.AuthorizationToken
+	tokenData, err := base64.StdEncoding.DecodeString(*authData.AuthorizationToken)
+	if err != nil {
+		return errors.Wrap(err, "Could not start docker cli")
+	}
 	endpoint := *authData.ProxyEndpoint
+	token := strings.SplitN(string(tokenData), ":", -1)[1]
 	argString := fmt.Sprintf("login --username AWS --password-stdin %s", endpoint)
-
-	cmd := exec.Command("docker", argString)
+	cmd := exec.Command("docker", strings.Fields(argString)...)
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		return errors.Wrap(err, "Couldn't open stdin to docker cli")
 	}
-
 	err = cmd.Start()
 	if err != nil {
 		return errors.Wrap(err, "Could not start docker cli")
 	}
 	_, err = io.WriteString(stdin, token)
 	if err != nil {
-		return errors.Wrap(err, "docker login failed")
+		return errors.Wrap(err, "couldnt write")
+	}
+	err = stdin.Close()
+	if err != nil {
+		return errors.Wrap(err, "couldnt close stdin")
 	}
 	err = cmd.Wait()
-	return errors.Wrap(err, "docker login failed")
+	return errors.Wrap(err, "couldnt wait")
 }
