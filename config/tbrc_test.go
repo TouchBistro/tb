8f3061135e3e034f114c3bd2791efdafdc34c9ff
Christopher Szatmary <cs@christopherszatmary.com>
2020-03-16 14:59:25 -0400

    chore: Test resolving registries when loading tbrc

--- config/tbrc_test.go
+++ config/tbrc_test.go
@@ -6,6 +6,8 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/TouchBistro/tb/registry"
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -34,6 +36,7 @@ func TestLoadTBRC(t *testing.T) {
 
 	assert.NoError(err)
 	assert.True(IsExperimentalEnabled())
+	assert.Equal(log.GetLevel(), log.DebugLevel)
 }
 
 func TestLoadTBRCDefault(t *testing.T) {
@@ -50,6 +53,7 @@ func TestLoadTBRCDefault(t *testing.T) {
 
 	assert.NoError(err)
 	assert.False(IsExperimentalEnabled())
+	assert.Equal(log.GetLevel(), log.InfoLevel)
 	assert.FileExists(tbrcPath)
 }
 
@@ -70,4 +74,41 @@ func TestLoadTBRCInvalidYaml(t *testing.T) {
 	err = LoadTBRC()
 
 	assert.Error(err)
+}
+
+func TestResolveRegistries(t *testing.T) {
+	assert := assert.New(t)
+	dir, err := setup()
+	if err != nil {
+		assert.FailNow("Failed to setup tmp dir", err)
+	}
+	defer os.RemoveAll(dir)
+
+	tbrcPath := filepath.Join(dir, tbrcName)
+	err = ioutil.WriteFile(tbrcPath, []byte(`registries:
+  - name: TouchBistro/tb-registry
+  - name: ExampleZone/tb-registry
+    localPath: ~/tools/tb-registry
+`), 0644)
+	if err != nil {
+		assert.FailNow("Failed to create tbrc file", err)
+	}
+
+	expectedRegistries := []registry.Registry{
+		registry.Registry{
+			Name:      "TouchBistro/tb-registry",
+			LocalPath: "",
+			Path:      filepath.Join(RegistriesPath(), "TouchBistro/tb-registry"),
+		},
+		registry.Registry{
+			Name:      "ExampleZone/tb-registry",
+			LocalPath: "~/tools/tb-registry",
+			Path:      filepath.Join(dir, "tools/tb-registry"),
+		},
+	}
+
+	err = LoadTBRC()
+
+	assert.NoError(err)
+	assert.ElementsMatch(expectedRegistries, Registries())
 }
