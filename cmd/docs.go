4442486c38ed3d2b4489981b65e45479a72b08f1
Ismail Arafa <ismailarafa18@gmail.com>
2020-05-29 21:21:47 -0400

    fix compiler errors

--- cmd/docs.go
+++ cmd/docs.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
   "fmt"
-  "sort"
 
   "github.com/TouchBistro/goutils/fatal"
   "github.com/TouchBistro/tb/config"
@@ -12,13 +11,17 @@ import (
 
 var docsCmd = &cobra.Command{
   Use:     "docs",
-  Args:    cobra.noArgs,
+	Args: cobra.MinimumNArgs(1),
   Short:   "Opens link to API docs for a given service",
   Run: func(cmd *cobra.Command, args []string) {
     serviceName := args[0]
-    service, err := config.LoadedServices.Get(serviceName)
+    service, err := config.LoadedServices().Get(serviceName)
     if err != nil {
       fatal.ExitErrf(err, "%s is not a valid service.\nTry running `tb list` to see available services\n", serviceName)
+    }
+
+    if !service.HasGitRepo() {
+			fatal.Exitf("%s does not have a repo or is a third-party repo\n", serviceName)
     }
 
     fmt.Println("Opening API docs for: %s", serviceName)
@@ -26,11 +29,10 @@ var docsCmd = &cobra.Command{
   },
 }
 
-func openDocs(service *service) {
-  fmt.Println("hals")
+func openDocs(s *service) {
+  fmt.Println("hals: %s", s.Name)
 }
 
 func init() {
   rootCmd.AddCommand(docsCmd)
-  upCmd.PersistentFlags().StringSliceVarP("service", "s", string, "service to lookup API docs for")
 }
