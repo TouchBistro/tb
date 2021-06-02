e1a520c5e8b97a22d61a694f53c872beab8791ca
Errol Cheong <echeong@touchbistro.com>
2021-06-02 02:41:52 -0400

    Get head SHA using git ls-remote

--- git/git.go
+++ git/git.go
@@ -1,7 +1,9 @@
 package git
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"path/filepath"
 
 	"github.com/TouchBistro/goutils/command"
@@ -25,6 +27,25 @@ func Pull(repo, repoDir string) error {
 		return errors.Wrapf(err, "failed to pull %s", repo)
 	}
 	return nil
+}
+
+func GetBranchHeadSha(repo, branch string) (string, error) {
+	repoUrl := "git@github.com:" + repo + ".git"
+	w := log.WithField("id", "git-ls-remote").WriterLevel(log.DebugLevel)
+	defer w.Close()
+	stdout := new(bytes.Buffer)
+	mw := io.MultiWriter(stdout, w)
+	cmd := command.New(command.WithStdout(mw), command.WithStderr(w))
+	err := cmd.Exec("git", "ls-remote", repoUrl, branch)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to get %s head sha of %s", branch, repo)
+	}
+
+	result := stdout.String()
+	if len(result) < 40 {
+		return "", errors.Errorf("ls-remote sha too short from %s of %s", branch, repo)
+	}
+	return result[0:40], nil
 }
 
 func execGit(id string, args ...string) error {
