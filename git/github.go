73cfec87b49d7be8bee8b5bb3851be729b2a9ca8
David Uzumeri <uzumerid@gmail.com>
2021-08-04 18:04:28 -0400

    Now using native GitHub client

--- git/github.go
+++ git/github.go
@@ -1,56 +1,19 @@
 package git
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+	"context"
 
+	"github.com/google/go-github/v37/github"
 	"github.com/pkg/errors"
 )
 
-const (
-	apiURL             = "https://api.github.com"
-)
-
 func GetLatestRelease() (string, error) {
-	url := fmt.Sprintf("%s/repos/TouchBistro/tb/releases/latest", apiURL)
-	client := &http.Client{}
+	client := github.NewClient(nil)
 
-	req, err := http.NewRequest("GET", url, nil)
+	release, _, err := client.Repositories.GetLatestRelease(context.Background(), "TouchBistro", "tb")
 	if err != nil {
-		return "", errors.Wrap(err, "Failed to create GET request to GitHub API")
+		return "", errors.Wrap(err, "Failed to retrieve latest release from GitHub")
 	}
 
-	// Use v3 API
-	req.Header.Add("Accept", "application/vnd.github.v3+json")
-
-	res, err := client.Do(req)
-	if err != nil {
-		return "", errors.Wrap(err, "Unable to get latest release of tb")
-	}
-
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return "", errors.Wrap(err, "Unable to read response body")
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return "", errors.Errorf("Got %d response from GitHub API:\n%s", res.StatusCode, string(body))
-	}
-
-	var jsonDict map[string]interface{}
-	err = json.Unmarshal(body, &jsonDict)
-	if err != nil {
-		return "", errors.Wrap(err, "Failed to parse JSON from reponse body")
-	}
-
-	tagName, ok := jsonDict["tag_name"].(string)
-	if !ok {
-		return "", errors.New("Unable to get tag name from response")
-	}
-
-	return tagName, nil
+	return *release.TagName, nil
 }
--- go.mod
+++ go.mod
@@ -4,18 +4,18 @@ go 1.16
 
 require (
 	github.com/TouchBistro/goutils v0.1.0
-	github.com/aws/aws-sdk-go-v2 v1.7.1
-	github.com/aws/aws-sdk-go-v2/config v1.5.0
+	github.com/aws/aws-sdk-go-v2 v1.8.0
+	github.com/aws/aws-sdk-go-v2/config v1.6.0
 	github.com/aws/aws-sdk-go-v2/service/ecr v1.4.1
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.11.1
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.12.0
 	github.com/blang/semver v3.5.1+incompatible
 	github.com/containerd/containerd v1.5.5 // indirect
-	github.com/docker/docker v20.10.7+incompatible
+	github.com/docker/docker v20.10.8+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/google/go-github/v37 v37.0.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.2.1
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
